9c1f0197b8f90f578a0e2be418253d9c75a748bf
NourAbdi <Dr.nour.3abdi@gmail.com>
2021-08-19 09:30:02 +0300

    release page

--- Numbine-master/src/app/demo/dell/configurator/pages/releases/releases.component.ts
+++ Numbine-master/src/app/demo/dell/configurator/pages/releases/releases.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DataTable } from '../../../models/DataTable';
+import { Product } from '../../../models/Product';
+import { Release } from '../../../models/Release';
+import { ProductService } from '../../../services/ProductService/product.service';
 
 @Component({
   selector: 'app-releases',
@@ -7,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReleasesComponent implements OnInit {
 
-  constructor() { }
+  trash: Boolean = false;
+  edit: Boolean = true;
+
+  public products: Product[] = [];
+
+  public title = ['SeqID', 'Param Name(^v)', 'Release', 'Version', 'Status'];
+  // 
+  public rows = [["Temp"], ["Temp"]];
+  public dataTable = new DataTable();
+  currentProduct: Product;
+  constructor(private productService: ProductService) {
+    // this.deleteFunction = this.deleteFunction.bind(this);
+    this.updateFunction = this.updateFunction.bind(this);
+  }
 
   ngOnInit(): void {
+    this.dataTable.rows = this.rows;
+    this.dataTable.titles = this.title;
+
+    let obsProducts = this.productService.getDummyProducts();
+    obsProducts.subscribe(products => {
+      this.products = products;
+      // Add loading?
+    }, error => {
+      alert("Error in loading products, product-release-mapping.component.ts");
+
+    });
+  }
+
+  updateRows() {
+    this.rows.length = 0;
+    this.products.forEach(element => {
+      this.products.forEach(element => {
+        this.rows.push(["element.productName"]);
+        this.rows.push(["element.releases"]);
+        this.rows.push(["element.status"]);
+      });
+    });
+  }
+  updateFunction(index: number)
+  {
+    alert("Updating item " + index);
   }
 
 }
